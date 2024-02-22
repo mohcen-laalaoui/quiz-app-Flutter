c4350b6d12909c420d8a964363391a91ee9a4eba
mohcen-laalaoui <mohcenlaa@gmail.com>
2024-02-22 23:13:57 +0100

    first commit

--- lib/quiz_brain.dart
+++ lib/quiz_brain.dart
@@ -8,6 +8,22 @@ class QuizBrain {
         q: 'Approximately one quarter of human bones are in the feet.',
         a: true),
     Question(q: 'A slug\'s blood is green.', a: true),
+    Question(
+        q: 'The Amazon Rainforest produces 20% of the world\'s oxygen',
+        a: false),
+    Question(q: 'Marie Curie discovered radium and polonium', a: true),
+    Question(q: 'The currency of Japan is called the yen', a: true),
+    Question(
+        q: 'Lionel Messi and Cristiano Ronaldo have both won the FIFA Ballon d\'Or award five times each.',
+        a: true),
+    Question(
+        q: 'Pele, widely regarded as one of the greatest football players of all time, won the FIFA World Cup three times with Brazil.',
+        a: true),
+    Question(
+        q: 'Lionel Messi has won the FIFA World Cup with Argentina.', a: true),
+    Question(
+        q: ' Cristiano Ronaldo has played for both Manchester United and Manchester City.',
+        a: false),
   ];
 
   void nextQuestion() {
@@ -31,10 +47,9 @@ class QuizBrain {
       return true;
       print('We\'re in the last question');
     }
-
   }
 
-  void reset(){
+  void reset() {
     _questionNumber = 0;
   }
 }
